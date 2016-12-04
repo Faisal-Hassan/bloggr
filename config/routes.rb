46259de742e6652c7207d085fe37a43172534d6c
Faisal Hassan <faisal.hassanx@gmail.com>
2016-12-04 05:14:20 +0000

    Create and style user sign up view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :users, except: [:new]
+  
+  get 'signup', to: 'users#new' 
+  
   resources :articles
+  
   get 'about', to: 'pages#about'
   
   root 'pages#home'
