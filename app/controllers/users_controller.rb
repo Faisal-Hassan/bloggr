e89113db636255c59af054888c812b8f5f2a6e89
Faisal Hassan <faisal.hassanx@gmail.com>
2016-12-06 03:07:32 +0000

    Fix nomethoderror for destroying users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,7 @@ class UsersController < ApplicationController
   end
   
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
     flash[:danger] = "User and all articles created by user have been deleted"
     redirect_to users_path
