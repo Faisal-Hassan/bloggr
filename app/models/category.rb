87637fd671d48b07e6bf8435961759e43f08b377
Faisal Hassan <faisal.hassanx@gmail.com>
2016-12-09 00:16:27 +0000

    Minor edits and remove search from nav

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ActiveRecord::Base
+  
   has_many :article_categories
   has_many :articles, through: :article_categories
   validates :name, presence: true, length: { maximum: 25, minimum: 4 }
   validates_uniqueness_of :name
+  
 end
