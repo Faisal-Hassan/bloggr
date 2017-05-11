e7ab838ee62ffe382c9f58b4fcd45b3da6e769a9
Faisal Hassan <faisal.hassanx@gmail.com>
2017-05-11 20:31:58 +0000

    Add custom controller actions to heart and unheart in hearts controller

--- app/controllers/hearts_controller.rb
+++ app/controllers/hearts_controller.rb
@@ -1,2 +1,17 @@
 class HeartsController < ApplicationController
+	respond_to :js
+	
+	def heart
+		@user = current_user
+		@article = Article.find(params[:article_id])
+		@user.heart!(@article)
+	end
+	
+	def unheart
+		@user = current_user
+		@heart = @user.hearts.find_by_article_id(params[:article_id])
+		@article = Article.find(params[:article_id])
+		@heart.destroy!
+	end
+	
 end
